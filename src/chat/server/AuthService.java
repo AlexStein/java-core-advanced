bcaa448485544388cee8c2401b45bed416853fa3
Alex Shevelev <alex.s@softmine.ru>
2020-09-12 17:02:17 +0300

    Lesson 8

--- src/chat/server/AuthService.java
+++ src/chat/server/AuthService.java
@@ -2,7 +2,10 @@ package chat.server;
 
 public interface AuthService {
     /**
+     * @return nickname если пользователь есть
      * @return null если пользоватаеля нет
-     */
+     * */
     String getNicknameByLoginAndPassword(String login, String password);
+
+    boolean registration(String login, String password, String nickname);
 }
